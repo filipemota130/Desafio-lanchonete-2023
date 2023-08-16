9df4a2c6b52a94c8fb2be9605594ef5f52d97da2
Filipe Simões Mota <fsmota@ic.ufal.br>
2023-08-16 16:34:40 -0300

    release: all tests + modularization function

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -2,54 +2,86 @@ import Lista_de_Produtos from './base-de-itens.js'
 import formas_de_pagamento from './base-de-pagamentos.js';
 
 class CaixaDaLanchonete {
-    calcularValorDaCompra(metodoDePagamento, itens){
-        let result = 0, forma, product, item_props, lista_prod = [];
+
+    productValidator(code){
         try{
-            forma = formas_de_pagamento[metodoDePagamento]
-            if (itens.length == 0) {
+            let product = Lista_de_Produtos[code]
+            return product
+        }
+        catch(e){
+            return false;
+        }
+    }
+    
+    paymentValidator(method){
+        try{
+            let forma = formas_de_pagamento[method]
+            return forma
+        }
+        catch(e){
+            return false;
+        }
+    }
+    
+    checkQuantity(quant){
+        if(quant < 1) {
+            return false
+        }
+        return quant
+    }
+    
+    MoneyFormater(amount, forma){
+        amount += (amount*forma.desc)
+        amount = amount.toFixed(2)
+        amount = amount.toString().replace('.', ',');
+        return "R$ "+ amount;
+    }
+    
+    calcularValorDaCompra(metodoDePagamento, itens){
+        let result = 0, forma, product, item_props, prod_list = [], code, quant , chec;
+        forma = this.paymentValidator(metodoDePagamento)
+        if(!forma){
+            result = "Forma de pagamento inválida!"
+        }
+        else {
+            if (itens.length === 0) {
                 result = "Não há itens no carrinho de compra!"
             }
             else{
                 for (let i = 0; i<itens.length; i++)
                 {
-                    item_props = itens[i].split(',')
-                    if (parseInt(item_props[1]) < 1) {
-                        result = "Quantidade inválida!"
-                        break
+                    item_props = itens[i].split(",")
+                    if (item_props.length <=1){
+
                     }
-                    try{
-                        product = Lista_de_Produtos[item_props[0]]
-                        lista_prod.push(item_props[0])
-                        console.log(product)
-                        if (product.extra){
-                            let search = lista_prod.includes(product.extra_de)
-                            if (search === false){
-                                result = "Item extra não pode ser pedido sem o principal"
-                                break
-                            }
-                        }
-                        result += product.valor * parseInt(item_props[1])
-                    }
-                    catch(e){
+                    code = item_props[0]
+                    
+                    product = this.productValidator(code)
+                    if (!product){
                         result = "Item inválido!"
                         break
                     }
+                    prod_list.push(code)
+                    if (product.extra){
+                        let search = prod_list.includes(product.extra_de)
+                        if (search === false){
+                            result = "Item extra não pode ser pedido sem o principal"
+                            break
+                        }
+                    }
+                    quant = this.checkQuantity(parseInt(item_props[1]))
+                    if (!quant){
+                        result = "Quantidade inválida!"
+                        break
+                    }
+                    result += product.valor * quant
                 }
-
             }
             if (typeof result === "number"){
-                result = result + (result*forma.desc)
-                result = result.toFixed(2)
-                result = result.toString().replace('.', ',');
-                return "R$ "+ result;
+                return this.MoneyFormater(result, forma)
             }
-            return result;
         }
-        catch(e){
-            result = "Forma de pagamento inválida!"
-            return result
-        }
+        return result;
     }
 }
-
 export { CaixaDaLanchonete };
